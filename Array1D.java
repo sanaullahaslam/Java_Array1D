daa74de52f7aeb1a455224944876fb6ba9ab87c9
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-02-12 11:18:36 -0800

    Add files via upload

---
+++ Array1D.java
@@ -0,0 +1,13 @@
+class Array1D {
+
+public static void main (String args []){
+
+String array[]={"Sanaullah","Sheeraz","Naveed","Najeeb","Hamid","Sir Fahad"};
+
+for(int i=0;i<=6;i++){	
+
+System.out.println(array[i]);
+
+	}	
+	}
+	}
